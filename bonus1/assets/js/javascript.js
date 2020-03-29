f2c50cf9022380ce05660afb4dd8ea2a69361a07
Alberto <alberto.germana@gmail.com>
2020-03-29 20:24:51 +0200

    Completato. + Refactoring fatto

--- bonus1/assets/js/javascript.js
+++ bonus1/assets/js/javascript.js
@@ -16,15 +16,18 @@ varie ed eventuali a fantasia */
 
 // dichiaro variabili che si collegano agli elementi input
 var userName = document.getElementById("userName");
-var userKm = document.getElementById("userKm");
+var userFrom = document.getElementById("from");
+var userTo = document.getElementById("to");
+var userKm; /* = document.getElementById("userKm"); */
 var eta = document.getElementById("eta");
 
 // dichiaro variabili che salveranno i valori degli input
-var userNameValue, userKmValue, etaValue;
+var userNameValue, userKmValue, etaValue, userFromValue, userToValue;
 
 // dichiaro variabili dei pulsanti che si collegano agli elementi button dell'html
 var generateButton = document.getElementById("genera");
 var resetButton = document.getElementById("reset");
+var buyButton = document.getElementById("buy");
 
 // variabili che con gli sconti
 var scontoUnder = 0.2;
@@ -33,20 +36,37 @@ var scontoOver = 0.4;
 /* Definizione del prezzo di 0.21€/km */
 var euroKm = 0.21;
 
-
+/* Funzione che al click del pulsante "Genera" raccoglie i dati dell'utente e crea un'anteprima del biglietto */
 generateButton.addEventListener("click", 
     function () {
-        // controllo se i campi sono vuoti
-        if((userName.value=="") || 
-            (userKm.value=="")){
-                alert("Hai dimenticato di compilare i campi");
+        // assegno i valori immessi nei campi nelle rispettive variabili
+        userNameValue = userName.value;
+        userFromValue = userFrom.value;
+        userToValue = userTo.value;
+        etaValue = eta.value;
+        /* Assegno ad ogni itinerario i Chilometri corrispondenti */
+        if(userNameValue==""){ // controllo se il campo "Nome e Cognome è compilato"
+                alert('Hai dimenticato di compilare il campo con "Cognome e Nome"');
         }else{
-            // assegno i valori immessi nei campi nelle rispettive variabili
-            userNameValue = userName.value;
-            userKmValue = userKm.value;
-            etaValue = eta.value;
+            if(userFromValue === userToValue){ // controllo se l'utente ha selezionato la stessa città tra Partenza e Arrivo
+                userKmValue = 0;
+            /* Da qui in poi assegno ad ogni itinerario i Km corrispondenti */
+            }else if(userFromValue === "palermo" && userToValue === "roma"){
+                userKmValue = 925;
+            }else if(userFromValue === "palermo" && userToValue === "milano"){
+                userKmValue = 1479;
+            }else if(userFromValue === "roma" && userToValue === "milano"){
+                userKmValue = 573;
+            }else if(userFromValue === "milano" && userToValue === "roma"){
+                userKmValue = 573;
+            }else if(userFromValue === "milano" && userToValue === "palermo"){
+                userKmValue = 1479;
+            }else if(userFromValue === "roma" && userToValue === "palermo"){
+                userKmValue = 925;
+            }
+        
     
-            // variabile che conterrà il costo del biglietto
+            // dichiaro variabile del biglietto scontato e valorizzo il costo del biglietto base
             var costoBiglietto = userKmValue * euroKm;
             var bigliettoScontato;
     
@@ -64,56 +84,70 @@ generateButton.addEventListener("click",
             
     
             // stampo i dati nell'HTML
-            document.getElementById("dettNome").innerHTML = "<span class='min'>Nome passeggero: </span><br>" + userNameValue;
-            document.getElementById("dettKm").innerHTML = "<span class='min'>Distanza: </span>" + userKmValue + "Km";
-            document.getElementById("dettEta").innerHTML = "<span class='min'>Età: </span>" + etaValue;
+            document.getElementById("dettNome").innerHTML = "<span class='min'>Nome passeggero: </span><br>" + userNameValue; //nome utente
+            document.getElementById("dettKm").innerHTML = "<span class='min'>Distanza: </span>" + userKmValue + "Km"; //distanza calcolata
+            document.getElementById("dettEta").innerHTML = "<span class='min'>Età: </span>" + etaValue; // fascia di età
+            document.getElementById("dettCosto").innerHTML = "<span class='min'>Costo del biglietto: </span><br>" + bigliettoScontato + "€"; // costo biglietto
+            document.getElementById("dettFrom").innerHTML = "<span class='min'>FROM: </span><br>" + userFromValue; //città di partenza
+            document.getElementById("dettTo").innerHTML = "<span class='min'>TO: </span><br>" + userToValue; //città di arrivo
+            // cambio classi nell'html per dinamicità del sito
+            document.getElementById("trainTicket").className = "displayYes"; //tolgo l'effetto sfocatura al biglietto
+            document.getElementById("genera").className = "buttonClass displayNo"; //nascondo tasto "Genera"
             
-            document.getElementById("dettCosto").innerHTML = "<span class='min'>Costo del biglietto: </span><br>" + bigliettoScontato + "€";
-    
-            document.getElementById("trainTicket").className = "displayYes";
-            document.getElementById("qr-code").className = "qr-code-displayed";
-
+            // lancio una funzione che crea un n° ID e la inietta nell'HTML
             document.getElementById("code").innerHTML = createTicketCode();
         }
-        
-        
-        
-
     }
     );
-    
+// Evento click per il tasto "Nuovo"
 resetButton.addEventListener("click", 
     function(){
-        // resetto i valori dei campi
+        // resetto il valore del campo del nome utente
         userName.value = "";
-        userKm.value = "";
-
+        
         //resetto html
         document.getElementById("dettNome").innerHTML = "<span class='min'>Nome passeggero: </span><br>";
         document.getElementById("dettKm").innerHTML = "<span class='min'>Distanza: </span>";
         document.getElementById("dettEta").innerHTML = "<span class='min'>Età: </span>";
-        
         document.getElementById("dettCosto").innerHTML = "<span class='min'>Costo del biglietto: </span><br>";
         document.getElementById("dettSconto").innerHTML = "";
-
-        document.getElementById("trainTicket").className = "displayNone";
-        document.getElementById("qr-code").className = "qr-code-none";
+        
+        // cambio classi nell'html per dinamicità del sito
+        document.getElementById("trainTicket").className = "displayNone"; //oscuro il biglietto
+        document.getElementById("qr-code").className = "qr-code-none"; // nascondo qr code
+        document.getElementById("code").className = "displayNo"; // nascondo codice biglietto
+        document.getElementById("buy").className = "displayBlock"; // mostro il link per acquistare il biglietto
+        document.getElementById("genera").className = "buttonClass"; // mostro il tasto "Genera"
     }
     
     );
-
+    // creo funzione per generare un ID del Biglietto
     function createTicketCode(){
-        var codice;
+        var codice = "ID:  ";
         var array = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0"];
-        var casuale = "ID:  "; 
+        var casuale; 
         
         for(var i = 0; i < 12; i++){
             casuale = Math.floor(Math.random() * array.length);
             codice = codice + array[casuale];
-            console.log("array[casuale]" + array[casuale]);
-            
-            
         }
-
         return codice;
     }
+// Evento Click per il link "Acquista"
+buyButton.addEventListener("click", 
+function(){
+    alert("Grazie per aver acquistato il biglietto. Trovi il numero identificativo della transazione sul biglietto. O per comodità puoi utilizzare il codice QR per oltrepassare i tornelli. Portalo sempre con te!")
+    // modifico classi html per dinamicità sito
+    document.getElementById("buy").className = "displayNo"; //nascondo il tasto "Acquista"
+    document.getElementById("code").className = "displayBlock"; //mostro codice id del biglietto
+    document.getElementById("qr-code").className = "qr-code-displayed"; // mostro codice qr
+}
+);
+
+// stampo data di oggi
+var today = new Date();
+var nowDay = today.getDate(); 
+var nowMonth = today.getMonth() + 1; 
+var nowYear = today.getFullYear(); 
+document.getElementById("now").innerHTML = nowDay + "/" + nowMonth + "/" + nowYear;
+document.getElementById("cost").innerHTML = euroKm;
